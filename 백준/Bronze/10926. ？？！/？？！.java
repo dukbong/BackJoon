dc8b458a1ac51788fac488aed4f02f4c166f5669
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-20 00:51:02 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        new Main().solution();    
+    }
+    
+    private void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine() + "??!";
+        
+        System.out.println(input);
+    }
+}
