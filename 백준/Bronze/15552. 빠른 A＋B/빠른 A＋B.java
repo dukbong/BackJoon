591c62b1bf17a8c22aaf67d429e39af520e9f9c7
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-19 16:47:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1184 ms, Memory: 300728 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+	}
+	
+	private void solution() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int count = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < count; i++){
+			int sum = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).reduce(0, (acc, cur) -> acc + cur);
+			bw.write(sum+"\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
