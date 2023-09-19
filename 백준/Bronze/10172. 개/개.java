898f1201511dc4fbd9c0ebcf4317baa550a53e18
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-20 00:52:15 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args){
+        new Main().createDog();
+    }
+    
+    private void createDog(){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
