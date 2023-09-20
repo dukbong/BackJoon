db1eefb29d204be5be99c5d935b2dc730fc39fb1
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-20 14:56:25 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+
+    private void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        String[] arr = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+        for(int i = 0; i < arr.length; i++){
+            if(str.contains(arr[i])) str = str.replace(arr[i]," ");
+        }
+        System.out.println(str.length());
+    }
+}
