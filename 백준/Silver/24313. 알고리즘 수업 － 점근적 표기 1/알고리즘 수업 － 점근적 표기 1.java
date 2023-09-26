de0287a3334b792dac7dfa717a851d5a60e9f660
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-26 11:06:43 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 128 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        String[] arr = br.readLine().split(" ");
+        int a1 = Integer.parseInt(arr[0]), a0 = Integer.parseInt(arr[1]);
+        int c = Integer.parseInt(br.readLine());
+        int n0 = Integer.parseInt(br.readLine());
+
+        int result = (a1 * n0 + a0 <= c * n0) && (c >= a1)?1:0;
+        System.out.println(result);
+    }
+}
