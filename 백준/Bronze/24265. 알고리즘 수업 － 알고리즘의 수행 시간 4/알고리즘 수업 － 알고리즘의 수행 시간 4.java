2630bdaf9d3d94a2712e35aed5d494baf608bc20
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-26 09:06:51 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+
+    private void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Integer.parseInt(br.readLine());
+        System.out.println((n*(n-1))/2);
+        System.out.println(2);
+    }
+}
