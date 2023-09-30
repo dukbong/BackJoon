2001eb3f6fae75b06ac242ad261e438eaae9fab9
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-30 19:51:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1440 ms, Memory: 129696 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+
+    private void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int num = Integer.parseInt(br.readLine());
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < num; i++) {
+        	list.add(Integer.parseInt(br.readLine()));
+        }
+        
+        Collections.sort(list);
+        
+        for(int i : list) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
