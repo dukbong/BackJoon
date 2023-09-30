b4319fc154b8df91606041593be266f2fb35bb99
Hyeon Sung Jang <gorae930301@gmail.com>
2023-09-30 20:24:32 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1072 ms, Memory: 78632 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        new Main().solution();
+    }
+
+    private void solution() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int[][] arr = new int[num][];
+        
+        for(int i = 0; i < num; i++) {
+        	int[] arr2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        	arr[i] = arr2;
+        }
+        
+        Arrays.sort(arr, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if(o1[1] == o2[1]) {
+					return o1[0] - o2[0];
+				}else {
+					return o1[1] - o2[1];
+				}
+			}
+		});
+        
+        for(int i = 0; i < arr.length; i++) {
+        	sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
