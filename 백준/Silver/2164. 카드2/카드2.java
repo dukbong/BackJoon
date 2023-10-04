d86de7de4c6161e9202f8917270a445a5a6c6dad
Hyeon Sung Jang <gorae930301@gmail.com>
2023-10-04 13:30:55 +0900

    [Silver IV] Title: 카드2, Time: 196 ms, Memory: 53468 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	ArrayList<Integer> list = new ArrayList<>();
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+	}
+	
+	private void solution() throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		for(int i = 1; i <= num; i++){
+			queue.offer(i);
+		}
+		while(queue.size() != 1){
+			queue.poll(); // 가장 윗장을 버린다.
+			int lastItem = queue.poll();
+			((LinkedList<Integer>) queue).addLast(lastItem);
+
+		}
+		System.out.println(queue.poll());
+	}
+}
