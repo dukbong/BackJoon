454f9934cd1c8f94cca9024b81a7c3e9d2a70c2a
Hyeon Sung Jang <gorae930301@gmail.com>
2023-10-25 10:42:00 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 980 ms, Memory: 108788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -2,41 +2,53 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+	
 	public static void main(String[] args) throws IOException {
 		new Main().solution();
 	}
-
+	
+	// 문재 : 점점 메모리와 시간은 줄지만 50점이다...
 	private void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		
 		String N = br.readLine();
 		int qCount = Integer.parseInt(br.readLine());
-		int[] sumArr = new int[N.length()];
-		String checkPoint = "";
-
+		// 누적합을 저장하는 Map
+		Map<String, int[]> map = new HashMap<>(); 
+		
 		for (int i = 0; i < qCount; i++) {
-			String[] arr = Arrays.stream(br.readLine().split(" ")).toArray(String[]::new);
-
-			if (!checkPoint.equals(arr[0])) { // 똑같다면 미리 찾아둔 값으로 해결한다.
-				checkPoint = arr[0];
-				sumArr[0] = String.valueOf(N.charAt(0)).equals(checkPoint) ? 1 : 0;
+			String[] arr = br.readLine().split(" ");
+			String key = arr[0];
+			int[] mapArr = map.get(key) == null ? new int[N.length()] : map.get(key);
+			
+			// 저장해둔다.=====================================================================================================
+			if (map.isEmpty() || !map.containsKey(key)) { 
+				// map에 key값이 존재하지 않는다면 저장한다.
+				map.put(key, mapArr); // 여기가 매번 최악의 경우를 생각한다.
+//				map.get(key)[0] = N.charAt(0) == key.charAt(0) ? 1 : 0;
+				mapArr[0] = oneOrZero(N,key,0);
 				for (int j = 1; j < N.length(); j++) {
-					sumArr[j] = sumArr[j - 1] + (String.valueOf(N.charAt(j)).equals(checkPoint) ? 1 : 0);
+					mapArr[j] = mapArr[j - 1] + oneOrZero(N,key,j);;
 				}
 			}
-
-			int check = machine(N, arr[0], Integer.parseInt(arr[1]), Integer.parseInt(arr[2]), sumArr);
+			// 저장해둔다.=====================================================================================================
+			
+			int check = machine(Integer.parseInt(arr[1]), Integer.parseInt(arr[2]), mapArr);
 			sb.append(check).append("\n");
 		}
-		System.out.println(sb.toString());
+		System.out.print(sb.toString());
+	}
+	
+	private int oneOrZero(String str1, String str2, int position){
+		return str1.charAt(position) == str2.charAt(0) ? 1 : 0;
 	}
 
-	private int machine(String string, String checkStr, int start, int end, int[] sumArr) {
-		if (start == 0) {
-			return sumArr[end];
-		}
-		return sumArr[end] - sumArr[start - 1];
+	private int machine(int start, int end, int[] sumArr) {
+		return sumArr[end] - (start == 0 ? 0 : sumArr[start - 1]);
 	}
 }
